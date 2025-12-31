58f11919c5a33e4599a94677be0934b7112a1d00
iefengyan <39669847+iefengyan@users.noreply.github.com>
2025-12-31 16:39:31 +0800

    Update __init__.py

--- models/mmseg/models/sam/__init__.py
+++ models/mmseg/models/sam/__init__.py
@@ -9,5 +9,3 @@ from .image_encoder import ImageEncoderViT
 from .mask_decoder import MaskDecoder
 from .prompt_encoder import PromptEncoder
 from .transformer import TwoWayTransformer
-from .mask_decoder_hq import MaskDecoderHQ
-from .image_encoder_ada import ImageEncoderViTAdapter
